c8a062af9d00aeaa43cff0414d7c14c2cb042ba8
zhihao <zhihao@DESKTOP-CTN5P3O.localdomain>
2021-12-15 19:05:46 -0500

    Submit the code

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,11 @@
+import pandas as pd
 
 def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+    writer.write(f"Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp\n")
+    df = pd.read_csv(reader)
+    df['date'] = pd.to_datetime(df['Measurement Timestamp']).dt.strftime('%m/%d/%Y')
+    df['Measurement Timestamp'] = pd.to_datetime(df['Measurement Timestamp'])
+    df = df.sort_values(['Station Name', 'Measurement Timestamp'])
+    df = df.groupby(['Station Name', 'date']).agg({'Air Temperature': ['min', 'max', 'first', 'last']})
+    for i_row in df.iterrows():
+        writer.write(f"{i_row[0][0]},{i_row[0][1]},{i_row[1][('Air Temperature', 'min')]},{i_row[1][('Air Temperature', 'max')]},{i_row[1][('Air Temperature', 'first')]},{i_row[1][('Air Temperature', 'last')]}\n")
--- test/weather_test.py
+++ test/weather_test.py
@@ -3,7 +3,29 @@ import io
 
 
 def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+    reader = io.StringIO("Station Name,Measurement Timestamp,Air Temperature\n\
+                         Foster Weather Station,01/01/2016 11:00:00 PM,69\n\
+Foster Weather Station,01/01/2016 08:00:00 PM,70\n\
+Foster Weather Station,01/01/2016 07:00:00 PM,70\n\
+Foster Weather Station,01/01/2016 06:00:00 PM,72\n\
+Foster Weather Station,01/01/2016 05:00:00 PM,72\n\
+Foster Weather Station,01/01/2016 04:00:00 PM,73\n\
+Foster Weather Station,01/01/2016 03:00:00 PM,69\n\
+Foster Weather Station,01/01/2016 02:00:00 PM,70\n\
+Foster Weather Station,01/01/2016 01:00:00 PM,70\n\
+Foster Weather Station,01/01/2016 12:00:00 PM,70\n\
+Foster Weather Station,01/01/2016 11:00:00 AM,70\n\
+Foster Weather Station,01/01/2016 10:00:00 AM,70\n\
+Foster Weather Station,01/01/2016 09:00:00 AM,70\n\
+Foster Weather Station,01/01/2016 08:00:00 AM,71\n\
+Foster Weather Station,01/01/2016 07:00:00 AM,72\n\
+Foster Weather Station,01/01/2016 06:00:00 AM,72\n\
+Foster Weather Station,01/01/2016 05:00:00 AM,71\n\
+Foster Weather Station,01/01/2016 04:00:00 AM,69\n\
+Foster Weather Station,01/01/2016 03:00:00 AM,67\n\
+Foster Weather Station,01/01/2016 02:00:00 AM,64\n\
+Foster Weather Station,01/01/2016 01:00:00 AM,67\n\
+Foster Weather Station,01/01/2016 12:00:00 AM,67\n")
     writer = io.StringIO()
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    assert writer.getvalue() is not None
